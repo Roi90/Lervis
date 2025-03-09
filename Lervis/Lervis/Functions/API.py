0e9f3c4842b550cb1dfd6df7b432dde21f92ce6c
Roi90 <rpf1990@gmail.com>
2025-03-09 19:05:52 +0000

    Testeo del GitIgnore

---
+++ Lervis/Lervis/Functions/API.py
@@ -0,0 +1,8 @@
+"""
+Este archivo contiene funciones para extraer datos de la API de arXiv.
+Proporciona métodos para buscar artículos, obtener detalles de los autores y
+filtrar resultados por fecha de publicación.
+
+Autor: [Tu Nombre]
+Fecha: [Fecha Actual]
+"""
