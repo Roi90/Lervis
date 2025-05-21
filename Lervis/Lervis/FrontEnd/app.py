76a1b5e086b2fecc611b1cea32444f1f8b525dd5
Roi90 <rpf1990@gmail.com>
2025-05-21 19:39:20 +0100

    Agregado en el end point save_context  rag_evaluator.

--- Lervis/Lervis/FrontEnd/app.py
+++ Lervis/Lervis/FrontEnd/app.py
@@ -2,16 +2,19 @@ from datetime import datetime, timedelta
 from flask import Flask, Response, request, render_template, stream_with_context, session
 from flask_session import Session
 from flask_sqlalchemy import SQLAlchemy
-
+from sklearn.metrics.pairwise import cosine_similarity
+from rouge_score import rouge_scorer
 
 
 from Functions.Chatbot_functions import  Llama3_1_API, RAG_chat_V2, actualizacion_informacion_inicial,  limitador_contexto
-from Functions.Embeddings import carga_BAAI
+from Functions.Embeddings import carga_BAAI, embedding_evaluator
 from Functions.Loggers import crear_logger
 from Functions.BBDD_functions import conn_bbdd
 from Functions.MarianMT_traductor import carga_modelo_traductor
 
 logger = crear_logger('Flask', 'Flask.log')
+rag_evaluator = crear_logger('rag_evaluator', 'rag_evaluator.log')
+
 conn = conn_bbdd()
 info_inicial = actualizacion_informacion_inicial()
 modelo_BAAI = carga_BAAI()
@@ -19,6 +22,8 @@ info_inicial = actualizacion_informacion_inicial()
 traductor_model, traductor_tokenizer = carga_modelo_traductor()
 
 logger = crear_logger('app', 'app.log')
+
+scorer = rouge_scorer.RougeScorer(['rouge1', 'rouge2', 'rougeL'], use_stemmer=True)
 
 urls_usados   = set()
 # variable que contiene la app Flask
@@ -117,7 +122,24 @@ def save_context():
     input_usuario = data['mensaje_usuario']
     respuesta_lervis = data['respuesta_lervis']
     
-    #print(f" Respuesta recibida en /save_context: {respuesta_lervis}")
+    # --- User Vs LLM -- Evaluacion
+    emb_usr_dense, emb_usr_sparse = embedding_evaluator(input_usuario, modelo_BAAI)
+    emb_llm_dense, emb_llm_sparse = embedding_evaluator(respuesta_lervis, modelo_BAAI)
+
+    user_set = set(emb_usr_sparse.keys())
+    llm_set = set(emb_llm_sparse.keys())
+
+    interseccion = llm_set.intersection(user_set)
+    union = llm_set.union(user_set)
+
+    # Metrica Lexica
+    similitud_jaccard = len(interseccion) / len(union)
+    # Metrica semantica
+    sim_semantica = cosine_similarity(emb_usr_dense.reshape(1, -1), emb_llm_dense.reshape(1, -1))[0][0]
+    # Metrica textual
+    scores = scorer.score(input_usuario, respuesta_lervis)
+
+    rag_evaluator.debug(f'Similitud coseno - {sim_semantica}, Similitud Jaccard - {similitud_jaccard}, Precision ROUGE 1 - {round(scores["rouge1"][0],2)}, Recall ROUGE 1 - {round(scores["rouge1"][1],2)}, Score ROUGE 1 - {round(scores["rouge1"][2],2)}, Precision ROUGE 2 - {round(scores["rouge2"][0],2)}, Recall ROUGE 2 - {round(scores["rouge2"][1],2)}, Score ROUGE 2 - {round(scores["rouge2"][2],2)}, Precision ROUGE L - {round(scores["rougeL"][0],2)}, Recall ROUGE L - {round(scores["rougeL"][1],2)}, Score ROUGE L - {round(scores["rougeL"][2],2)}')
 
     ahora = datetime.utcnow().strftime("%d/%m/%Y %H:%M")
     # Si no hay contexto se crea uno nuevo junto con la informacion inicial
